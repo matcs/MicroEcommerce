8572ae01876e0b7128fc72dd1da20a3b168f384b
Matheus <matheuscostados.santos18@gmail.com>
2020-11-18 12:16:18 -0300

    Products Catalog Service

---
+++ ProductCatalog/Data/ApplicationDbContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+               .UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CatalogDb;Trusted_Connection=True;",
+               providerOptions => providerOptions.CommandTimeout(100));
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product()
+                {
+                    ProductId = 1,
+                    Name = "Cadeira da jantar",
+                    Description = "Uma cadeira feita para jantar",
+                    Image = Product.ReadFile("Images\\chair.jpg")
+                },
+                new Product()
+                {
+                    ProductId = 2,
+                    Name = "Cadeira gamer",
+                    Description = "Uma cadeira feita para jogar",
+                    Image = Product.ReadFile("Images\\gamer-chair.jpg")
+                },
+                new Product()
+                {
+                    ProductId = 3,
+                    Name = "Cadeira de varanda",
+                    Description = "Uma cadeira feita para por na varanda",
+                    Image = Product.ReadFile("Images\\chair-varanda.jpg")
+                },
+                new Product()
+                {
+                    ProductId = 4,
+                    Name = "Cadeira de escritorio",
+                    Description = "Uma cadeira feita para por no escritorio",
+                    Image = Product.ReadFile("Images\\chair-escritorio.jpg")
+                });
+
+
+        }
+    }
+}
---
+++ ProductCatalog/Models/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+        public string Description { get; set; }
+
+        public static byte[] ReadFile(string sPath)
+        {
+            byte[] data = null;
+
+            FileInfo fInfo = new FileInfo(sPath);
+            long numBytes = fInfo.Length;
+
+            FileStream fStream = new FileStream(sPath, FileMode.Open, FileAccess.Read);
+
+            BinaryReader br = new BinaryReader(fStream);
+
+            data = br.ReadBytes((int)numBytes);
+
+            return data;
+        }
+
+        public Product() { }
+
+        public Product(int productId, string name, byte[] image, string description)
+        {
+            ProductId = productId;
+            Name = name;
+            Image = image;
+            Description = description;
+        }
+    }
+}
